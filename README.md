1981533fae74e4fa85334a77158719e74026e875
Alain Armand <aarmand.inbox@gmail.com>
2020-12-06 01:15:55 +0100

    port to fish:WIP

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # fromhex
 
 [`fromhex`](https://github.com/peterpme/dotfiles/blob/master/bin/fromhex) wrapper for fish using [bass](https://github.com/edc/bass).
+## ExplainShell
+
+https://explainshell.com/explain?cmd=hex%3D%24%7B1%23%22%23%22%7D
 
 ## Installation
 
--- functions/fromhex.fish
+++ functions/fromhex.fish
@@ -1,4 +1,11 @@
 #!/usr/bin/env fish
-set script_dir (realpath (dirname (status --current-filename)))
-
-bass source $script_dir/fromhex
+# no idea if this works yet
+function fromhex
+    set hex {1#"#"}
+    set r (printf '0x%0.2s' $hex)
+    set g (printf '0x%0.2s' $hex#??)
+    set b (printf '0x%0.2s' $hex#????)
+    printf '%03d' "((
+  ($r<75?0:($r-35)/40)*6*6 + ($g<75?0:($g-35)/40)*6 + ($b<75?0:($b-35)/40) + 16
+))"
+end
